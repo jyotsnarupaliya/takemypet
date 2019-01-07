be2c04ef74448cd090c628563d84833ff31c31a1
Jyotsna <jagjyotsna@gmail.com>
2019-01-07 21:33:19 +0530

    added api

--- takemypet/petstore/views.py
+++ takemypet/petstore/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import pet, owner
+from django.template import loader	
 from rest_framework import viewsets
+from django.http import HttpResponse
 from petstore.serializers import petSerializer
 
 # Create your views here.
 class PetViewSet(viewsets.ModelViewSet):
 	queryset = pet.objects.all()
 	serializer_class = petSerializer
+
+
+
+def home(request):
+	template = loader.get_template('petstore/allpets.html')
+	return HttpResponse(template.render({}, request))
 	
